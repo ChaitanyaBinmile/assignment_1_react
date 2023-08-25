6f272862e03aa34b0f041c78bc89965926c94a67
Chaitanya <chaitanya.v@binmile.com>
2023-08-25 15:39:14 +0530

    final

--- react-figma/src/components/iconComponent/Icon.tsx
+++ react-figma/src/components/iconComponent/Icon.tsx
@@ -4,7 +4,7 @@ import Menu from "../menuComponent/desktop_menuComponent/Menu"
 
 function Icon() {
   return (
-    <div className='flex justify-center bg-icon text-center gap-10 items-center '>
+    <div className='icon'>
       
      <IconImg/>
       
