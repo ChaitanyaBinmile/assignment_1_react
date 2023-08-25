97ea56208a4d58878e74d234c7e5b661c4fc7d79
Chaitanya <chaitanya.v@binmile.com>
2023-08-25 16:11:40 +0530

    _

--- react-figma/src/pages/Home.tsx
+++ react-figma/src/pages/Home.tsx
@@ -5,10 +5,10 @@ import {Left,Header,Right} from "../utilities/export_file"
 function Home() {
   return (
     <div className='appCSS'>
-      <Header></Header>
+      <Header/>
       <div className='app_componentCSS'>
-        <Left></Left>
-        <Right></Right>
+        <Left/>
+        <Right/>
 
       </div>
 
